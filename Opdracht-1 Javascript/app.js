ce98679706258271cd61fd06ba063887b95d60fc
MatthiasvdMeer <9026846@student.tcrmbo.nl>
2024-11-28 12:09:01 +0100

    versie 1

---
+++ Opdracht-1 Javascript/app.js
@@ -0,0 +1,8 @@
+ // Functie om de cookie-muur te accepteren
+ function acceptCookies() {
+  // Verberg de cookie-muur
+  document.getElementById("cookieWall").style.display = "none";
+  
+  // Toon de content
+  document.getElementById("content").style.display = "block";
+}
